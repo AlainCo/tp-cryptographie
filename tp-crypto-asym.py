151a8da562e6038743c50454213ddd7902b43e7d
Zelaryon <216882118+zelaryon@users.noreply.github.com>
2025-06-23 16:00:06 +0200

    cas alice et bob chiffrement

--- tp-crypto-asym.py
+++ tp-crypto-asym.py
@@ -13,6 +13,13 @@ SIGNED_FILE_NAME = "work/signed.bin"
 SIGNATURE_FILE_NAME = "work/signature.bin"
 PRIVATE_KEY_FILE = "work/private_key.pem"
 PUBLIC_KEY_FILE = "work/public_key.pem"
+
+ALICE_PRIVATE_KEY_FILE = "work/alice.private_key.pem"
+ALICE_PUBLIC_KEY_FILE = "work/alice.public_key.pem"
+BOB_PRIVATE_KEY_FILE = "work/bob.private_key.pem"
+BOB_PUBLIC_KEY_FILE = "work/bob.public_key.pem"
+
+
 KEY_SIZE = 2048  # Taille de la clé RSA
 SYMMETRIC_KEY_SIZE = 32  # Taille de la clé symétrique AES-256
 
@@ -34,7 +41,7 @@ def generate_rsa_keys(private_key_file,public_key_file):
             encoding=serialization.Encoding.PEM,
             format=serialization.PublicFormat.SubjectPublicKeyInfo
         ))
-    print(f"Clés RSA sauvegardées vers  {PRIVATE_KEY_FILE} et {PUBLIC_KEY_FILE}")
+    print(f"Clés RSA sauvegardées vers  {private_key_file} et {public_key_file}")
     return private_key, public_key
 
 def load_rsa_private_key(private_key_file):
@@ -45,7 +52,7 @@ def load_rsa_private_key(private_key_file):
             password=None,
             backend=default_backend()
         )
-    print(f"Clés privée RSA chargée depuis {PRIVATE_KEY_FILE} ")
+    print(f"Clés privée RSA chargée depuis {private_key_file} ")
     return private_key
 def load_rsa_public_key(public_key_file):
     # Charger la clé publique
@@ -54,7 +61,7 @@ def load_rsa_public_key(public_key_file):
             f.read(),
             backend=default_backend()
         )
-    print(f"Clés publique RSA chargée depuis {PUBLIC_KEY_FILE}")
+    print(f"Clés publique RSA chargée depuis {public_key_file}")
     return public_key
 
 def encrypt_file_rsa(input_file, output_file, public_key):
@@ -147,7 +154,7 @@ def verify_signature(input_file, signature_file, public_key):
     except:
         return False
 
-def main():
+def test_simple():
     # Générer les clés RSA, si besoin
     if os.path.exists(PRIVATE_KEY_FILE) and os.path.exists(PUBLIC_KEY_FILE):
         #charger les clés RSA
@@ -175,9 +182,32 @@ def main():
         print("La signature est valide.")
     else:
         print("La signature est invalide.")
+        
+def test_alice_bob():
+    if os.path.exists(ALICE_PRIVATE_KEY_FILE) and os.path.exists(ALICE_PUBLIC_KEY_FILE):
+        #charger les clés RSA
+        alice_public_key = load_rsa_public_key(ALICE_PUBLIC_KEY_FILE)
+        alice_private_key = load_rsa_private_key(ALICE_PRIVATE_KEY_FILE)
+    else:
+        alice_private_key, alice_public_key = generate_rsa_keys(ALICE_PRIVATE_KEY_FILE,ALICE_PUBLIC_KEY_FILE)
+    if os.path.exists(BOB_PRIVATE_KEY_FILE) and os.path.exists(BOB_PUBLIC_KEY_FILE):
+        #charger les clés RSA
+        bob_public_key = load_rsa_public_key(BOB_PUBLIC_KEY_FILE)
+        bob_private_key = load_rsa_private_key(BOB_PRIVATE_KEY_FILE)
+    else:
+        bob_private_key, bob_public_key = generate_rsa_keys(BOB_PRIVATE_KEY_FILE,BOB_PUBLIC_KEY_FILE)
+    # je suis alice
+    # Chiffrer le fichier avec la clé publique RSA de BOB et une clé symétrique AES aléatoire puis chiffrée
+    encrypt_file_rsa(FILE_NAME, ENCRYPTED_FILE_NAME, bob_public_key)
+    print(f"Fichier {FILE_NAME} chiffré en {ENCRYPTED_FILE_NAME}")
+    
+    # je suis bob
+    # Déchiffrer le fichier avec la clé privée RSA de BOB et la clé symétrique AES chiffére dans le fichier
+    decrypt_file_rsa(ENCRYPTED_FILE_NAME, DECRYPTED_FILE_NAME, bob_private_key)
+    print(f"Fichier {ENCRYPTED_FILE_NAME} déchiffré en {DECRYPTED_FILE_NAME}")
 
 if __name__ == "__main__":
     try:
-        main()
+        test_alice_bob()
     except Exception as e:
         print(f"exception : {e}")
